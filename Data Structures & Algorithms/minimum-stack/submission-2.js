0e73cf14ba08a9e89d97e0171c26140849b4692e
ImranJalal <109538457+ImranJalal@users.noreply.github.com>
2026-05-06 16:47:57 -0400

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.js
@@ -0,0 +1,45 @@
+class MinStack {
+    constructor(name) {
+        this.name = name;
+        this.stack = [];
+        this.minStack = [];
+        this.length = 0;
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+        if(this.length == 0){
+            this.minStack.push(val)
+        } else {
+            this.minStack.push(Math.min(val, this.minStack[this.length-1]))
+        }
+        this.length++
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop();
+        this.minStack.pop();
+        this.length--
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.length-1]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minStack[this.length-1]
+    }
+}
