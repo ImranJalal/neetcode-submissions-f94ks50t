c64e43a88caf0d661b952d7f3c01f4b77c7ccea7
ImranJalal <109538457+ImranJalal@users.noreply.github.com>
2026-05-11 21:09:26 -0400

    Add: merge-two-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-0.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} list1
+     * @param {ListNode} list2
+     * @return {ListNode}
+     */
+    mergeTwoLists(list1, list2) {
+        let dummy = new ListNode(0);
+        let current = dummy
+    while(list1 && list2){
+        if(list1.val < list2.val){
+            current.next = list1; // your reversing the pointers so list1 starts at null for a new list
+            list1 = list1.next; // list 1 equal list1.next skips over to the next point
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next; // in the new list after running the conditional you want to skip to next point 
+    }
+        current.next = list1 || list2; // attach the remaining list either list1 or list2
+
+        return dummy.next
+    }
+}
